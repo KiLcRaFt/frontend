9e9dfeb160df0795617335703875b20d4442ece3
opilane <killcraft96@gmail.com>
2024-10-15 14:41:11 +0300

    On tehtud kõik harjutused, mis oli postiautomaatide ees

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tooded, setTooded] = useState([]);
+  const idRef = useRef();
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const isActiveRef = useRef();
+
+  useEffect(() => {
+    fetch("https://localhost:7094/tooded")
+        .then(res => res.json())
+        .then(json => setTooded(json));
+  }, []);
+
+  //////////////////////////////////////////////-->
+
+  function kustuta(index) {
+    fetch("https://localhost:7094/tooded/kustuta/" + index, {"method": "DELETE"})
+        .then(res => res.json())
+        .then(json => setTooded(json));
+  }
+
+  //////////////////////////////////////////////-->
+
+  function lisa() {
+    const uusToode = {
+      "id": Number(idRef.current.value),
+      "name": nameRef.current.value,
+      "price": Number(priceRef.current.value),
+      "isActive": isActiveRef.current.checked
+    }
+    fetch("https://localhost:7094/tooded/lisa", {"method": "POST", "body": JSON.stringify(uusToode)})
+        .then(res => res.json())
+        .then(json => setTooded(json));
+  }
+
+  //////////////////////////////////////////////-->
+
+  function dollariteks() {
+    const kurss = 1.1;                                      ////////////////////////
+    fetch("https://localhost:7094/tooded/hind-dollaritesse/" + kurss, {"method": "PATCH"})
+        .then(res => res.json())
+        .then(json => setTooded(json));
+  }
+
+  //////////////////////////////////////////////-->
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <label>ID</label> <br />
+        <input ref={idRef} type="number" /> <br />
+        <label>Nimi</label> <br />
+        <input ref={nameRef} type="text" /> <br />
+        <label>Hind</label> <br />
+        <input ref={priceRef} type="number" /> <br />
+        <label>Aktiivne</label> <br />
+        <input ref={isActiveRef} type="checkbox" /> <br />
+        <button onClick={() => lisa()}>Lisa</button>
+        {tooded.map((toode, index) =>
+            <div>
+              <div>{toode.id}</div>
+              <div>{toode.name}</div>
+              <div>{toode.price}</div>
+              <button onClick={() => kustuta(index)}>x</button>
+            </div>)}
+        <button onClick={() => dollariteks()}>Muuda dollariteks</button>
+      </div>
   );
 }
 
